e6a888f0811ea7f57fd5cd62f61a9b70c728e75f
Andy Philpotts <andy@failfree.net>
2021-08-03 21:11:00 -0400

    added animation on click

--- src/main.rs
+++ src/main.rs
@@ -86,13 +86,42 @@ impl AppData {
     }
 }
 
-struct TablatureWidget {}
+struct TablatureWidget {
+    t: f64,
+
+    image_width: usize,
+    image_height: usize,
+    image_data: Vec<u8>,
+}
 
 impl TablatureWidget {
     pub const STRING_COLOR: Color = Color::rgb8(0, 128, 0);
 
+    fn get_image_data(&mut self, width: usize, height: usize) -> &Vec<u8> {
+        if width != self.image_width || height != self.image_height {
+            let mut result = vec![0; width * height * 4];
+            for y in 0..height {
+                for x in 0..width {
+                    let ix = (y * width + x) * 4;
+                    result[ix] = x as u8;
+                    result[ix + 1] = y as u8;
+                    result[ix + 2] = !(x as u8);
+                    result[ix + 3] = 127;
+                }
+            }
+            self.image_data = result;
+        }
+
+        &self.image_data
+    }
+
     pub fn new() -> TablatureWidget {
-        TablatureWidget {}
+        TablatureWidget {
+            t: 0.0,
+            image_width: 0,
+            image_height: 0,
+            image_data: Vec::new(),
+        }
     }
 }
 
@@ -100,8 +129,20 @@ impl TablatureWidget {
 // (and lifecycle) methods as well to make sure it works. Some things can be filtered,
 // but a general rule is to just pass it through unless you really know you don't want it.
 impl Widget<AppData> for TablatureWidget {
-    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut AppData, _env: &Env) {}
-
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, _data: &mut AppData, _env: &Env) {
+        match event {
+            Event::MouseDown(_) => {
+                self.t = 0.0;
+                ctx.request_anim_frame();
+            }
+            Event::AnimFrame(interval) => {
+                ctx.request_paint();
+                self.t += (*interval as f64) * 1e-9;
+                ctx.request_anim_frame();
+            }
+            _ => (),
+        }
+    }
     fn lifecycle(
         &mut self,
         _ctx: &mut LifeCycleCtx,
@@ -151,7 +192,7 @@ impl Widget<AppData> for TablatureWidget {
         let rect = size.to_rect();
         ctx.fill(rect, &Color::WHITE);
 
-        for bar in data.bars.iter() {}
+        let timex = self.t * 100.0;
 
         let line_delta = size.height / 7.0;
         let mut line_y = line_delta; // Kinda hack, should be neck margin
@@ -176,8 +217,9 @@ impl Widget<AppData> for TablatureWidget {
         // This is one way of doing it, you can also use a builder-style way.
         let mut layout = new_text_layout("Whoops no data");
         layout.rebuild_if_needed(ctx.text(), env);
-        for i in 0..10 {
-            let x = i as f64;
+
+        for bar in data.bars.iter().enumerate() {
+            let x = bar.0 as f64;
             layout.draw(ctx, (80.0 + (x * 10.0), 40.0 + (x * 15.0)));
         }
 
@@ -202,94 +244,23 @@ impl Widget<AppData> for TablatureWidget {
             .text_color(Color::rgb8(128, 0, 0))
             .build()
             .unwrap();
-        ctx.draw_text(&layout, (100.0, 25.0));
+        ctx.draw_text(&layout, (100.0 + timex, 25.0));
 
         // Let's burn some CPU to make a (partially transparent) image buffer
-        let image_data = make_image_data(256, 256);
+        let image_data = self.get_image_data(256, 256);
         let image = ctx
-            .make_image(256, 256, &image_data, ImageFormat::RgbaSeparate)
+            .make_image(256, 256, image_data, ImageFormat::RgbaSeparate)
             .unwrap();
         // The image is automatically scaled to fit the rect you pass to draw_image
         ctx.draw_image(&image, size.to_rect(), InterpolationMode::Bilinear);
     }
-
-    // // The paint method gets called last, after an event flow.
-    // // It goes event -> update -> layout -> paint, and each method can influence the next.
-    // // Basically, anything that changes the appearance of a widget causes a paint.
-    // fn _paint_old(&mut self, ctx: &mut PaintCtx, data: &String, env: &Env) {
-    //     // Clear the whole widget with the color of your choice
-    //     // (ctx.size() returns the size of the layout rect we're painting in)
-    //     // Note: ctx also has a `clear` method, but that clears the whole context,
-    //     // and we only want to clear this widget's area.
-    //     let size = ctx.size();
-    //     let rect = size.to_rect();
-    //     ctx.fill(rect, &Color::WHITE);
-
-    //     let line_delta = size.height / 7.0;
-    //     let mut line_y = line_delta; // Kinda hack, should be neck margin
-
-    //     for _i in 0..6 {
-    //         ctx.stroke(
-    //             Line::new((0.0, line_y), (size.width, line_y)),
-    //             &Self::STRING_COLOR,
-    //             4.0,
-    //         );
-    //         line_y += line_delta;
-    //     }
-
-    //     // Rectangles: the path for practical people
-    //     let nut_rect = Rect::new(0.0, 0.0, line_delta / 5.0, size.height);
-    //     // My nuts are black
-    //     let fill_color = Color::rgb8(0x00, 0x00, 0x00);
-    //     ctx.fill(nut_rect, &fill_color);
-
-    //     // Text is easy; in real use TextLayout should either be stored in the
-    //     // widget and reused, or a label child widget to manage it all.
-    //     // This is one way of doing it, you can also use a builder-style way.
-    //     let mut layout = new_text_layout(data);
-    //     layout.rebuild_if_needed(ctx.text(), env);
-    //     for i in 0..10 {
-    //         let x = i as f64;
-    //         layout.draw(ctx, (80.0 + (x * 10.0), 40.0 + (x * 15.0)));
-    //     }
-
-    //     // Let's rotate our text slightly. First we save our current (default) context:
-    //     ctx.with_save(|ctx| {
-    //         // Now we can rotate the context (or set a clip path, for instance):
-    //         // This makes it so that anything drawn after this (in the closure) is
-    //         // transformed.
-    //         // The transformation is in radians, but be aware it transforms the canvas,
-    //         // not just the part you are drawing. So we draw at (80.0, 40.0) on the rotated
-    //         // canvas, this is NOT the same position as (80.0, 40.0) on the original canvas.
-    //         ctx.transform(Affine::rotate(std::f64::consts::FRAC_PI_4));
-    //         layout.draw(ctx, (80.0, 40.0));
-    //     });
-    //     // When we exit with_save, the original context's rotation is restored
-
-    //     // This is the builder-style way of drawing text.
-    //     let text = ctx.text();
-    //     let layout = text
-    //         .new_text_layout(data.clone())
-    //         .font(FontFamily::SERIF, 24.0)
-    //         .text_color(Color::rgb8(128, 0, 0))
-    //         .build()
-    //         .unwrap();
-    //     ctx.draw_text(&layout, (100.0, 25.0));
-
-    //     // Let's burn some CPU to make a (partially transparent) image buffer
-    //     let image_data = make_image_data(256, 256);
-    //     let image = ctx
-    //         .make_image(256, 256, &image_data, ImageFormat::RgbaSeparate)
-    //         .unwrap();
-    //     // The image is automatically scaled to fit the rect you pass to draw_image
-    //     ctx.draw_image(&image, size.to_rect(), InterpolationMode::Bilinear);
-    // }
 }
 
 pub fn main() {
     let window = WindowDesc::new(TablatureWidget::new()).title(LocalizedString::new("fret"));
+
     AppLauncher::with_window(window)
-        .use_simple_logger()
+        .log_to_console()
         .launch(AppData::new())
         .expect("launch failed");
 }
@@ -300,18 +271,4 @@ fn new_text_layout(data: &str) -> TextLayout<String> {
     layout.set_font(FontDescriptor::new(FontFamily::SERIF).with_size(24.0));
     layout.set_text_color(fill_color);
     layout
-}
-
-fn make_image_data(width: usize, height: usize) -> Vec<u8> {
-    let mut result = vec![0; width * height * 4];
-    for y in 0..height {
-        for x in 0..width {
-            let ix = (y * width + x) * 4;
-            result[ix] = x as u8;
-            result[ix + 1] = y as u8;
-            result[ix + 2] = !(x as u8);
-            result[ix + 3] = 127;
-        }
-    }
-    result
 }
